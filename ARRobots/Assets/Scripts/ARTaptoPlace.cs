179252f092089082daaa8da85c1f265f0cb97939
jieguann <60665347+jieguann@users.noreply.github.com>
2021-11-02 22:33:23 -0400

    Update ARTaptoPlace.cs

--- ARRobots/Assets/Scripts/ARTaptoPlace.cs
+++ ARRobots/Assets/Scripts/ARTaptoPlace.cs
@@ -9,15 +9,25 @@ public class ARTaptoPlace : MonoBehaviour
     public GameObject spawnPrefeb; //to be spawn (instantiated)
     private ARRaycastManager raycastManager;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        raycastManager = GetComponent<ARRaycastManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.touchCount > 0) //Identify the number of touches
+        {
+            Vector2 touchPosition = Input.GetTouch(0).position;
+            if(raycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+            {
+                var hitPose = hits[0].pose;
+
+                Instantiate(spawnPrefeb, hitPose.position, hitPose.rotation);
+            }
+        }
     }
 }
